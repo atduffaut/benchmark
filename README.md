d94509fd441f7e621eedb7a268a2ba56311771e0
atduffaut <alex.duffaut@gmail.com>
2023-04-23 16:09:57 -0400

    Run processes on same core

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Context-Switching Benchmark
 Simple program made to emulate a context-switch between a parent and child process, blocking and switching while they share information between two pipes.
-This emulation shows the time it takes an operating system to save the registers for one process, and restore the registers for another. 
-The program displays time elapsed in miliseconds, and runs until it is killed.
+This emulation shows the time it takes an operating system to save the registers for one process, and restore the registers for another.
+This is shown by running two processes, parent and child, on the same CPU core, and sending a sort of "go" signal between the two, and timing the context switch that occurs for the core to switch between the two processes.
+The program displays time elapsed in miliseconds, and runs until it is killed (with Ctrl+C).
+
+Note: MacOS does not allow setting processes to specific cores, so the Linux syscalls `sched_getaffinity()` and `sched_setaffinity()` are abstracted in the top of the program. If running on MacOS, leave those functions in the code. If using Linux, comment them out so the true syscalls can be made, and the processes will be set to the same core.
+
 To use, simply run:
 ```
 make
--- benchmark.cpp
+++ benchmark.cpp
@@ -6,9 +6,60 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <iostream>
 
+
+// COMMENT OUT THESE FUNCTIONS IMPLEMENTATIONS TO EXECUTE ON LINUX
+
+// Unix implementation of the Linux syscall "sched_getaffinity"
+// and "sched_setaffinity"
+
+#define SYSCTL_CORE_COUNT   "machdep.cpu.core_count"
+
+typedef struct cpu_set {
+  uint32_t    count;
+} cpu_set_t;
+
+static inline void
+CPU_ZERO(cpu_set_t *cs) { cs->count = 0; }
+
+static inline void
+CPU_SET(int num, cpu_set_t *cs) { cs->count |= (1 << num); }
+
+static inline int
+CPU_ISSET(int num, cpu_set_t *cs) { return (cs->count & (1 << num)); }
+
+int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set)
+{
+  int32_t core_count = 0;
+  size_t  len = sizeof(core_count);
+  int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
+  if (ret) {
+    printf("error while get core count %d\n", ret);
+    return -1;
+  }
+  cpu_set->count = 0;
+  for (int i = 0; i < core_count; i++) {
+    cpu_set->count |= (1 << i);
+  }
+
+  return 0;
+} // Simplified version of Linux syscall to retrieve number of cores
+
+int sched_setaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set) {
+    return 0;
+}; // No MacOS APIs exist to implement this Linux syscall
+
 int main() {
+
+    // Set CPU 0
+    cpu_set_t set;
+    CPU_ZERO(&set);
+    CPU_SET(0, &set);
+
     //creating two pipes
     int p1[2]; // parent -> child
     int p2[2]; // child -> parent
@@ -20,6 +71,10 @@ int main() {
     if (rc < 0) exit(3);
     else if (rc == 0) {
         //in child process
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            exit(3);
+        }
+
         close(p1[1]);
         close(p2[0]);
         while(true) {
@@ -33,6 +88,9 @@ int main() {
 
     else {
         //in parent, this executes first
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            exit(4);
+        }
         close(p1[0]);
         close(p2[1]);
         struct timeval current_time;
